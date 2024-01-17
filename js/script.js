228d6848ae933c61e77a32f45129e7e666ebe0b4
breathingstyle7 <romagolembiovski@gmail.com>
2024-01-17 23:48:48 +0200

    а

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let userSolary = prompt("Введіть розмір вашої заробітної плати")
+userSolary = parseFloat(userSolary)
+
+console.log(userSolary)
+
+// хочу записати деякий текст ще в додачу до цього
+
+const award = userSolary * (15/100)
+
